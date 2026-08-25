2307ccd8cd7f91eceafb4ee35d6e4035507c869f
unknown <23ai009@sitmng.ac.in>
2026-08-25 23:15:29 +0530

    add the startserver which will start the server and handle the graceful shutdown

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ import logger from "./shared/config/logger.js";
 import errorHabler from "./shared/middlewares/errorHandler.js";
 import mongodb from "./shared/config/mongodb.js";
 import rabbitmq from "./shared/config/rabbitmq.js";
+import postgres from "./shared/config/postgres.js";
+import config from "./shared/config/index.js";
 
 dotenv.config();
 
@@ -46,55 +48,100 @@ app.get("/health", (req, res) => {
   );
 });
 
+app.get("/", (req, res) => {
+  res.status(200).json(
+    ResponceFormatter.sucess({
+      service: "Api Monitoring Service",
+      version: "1.0.0",
+      endpoints: [
+        {
+          health: "/health",
+          auth: "/api/auth",
+          ingest: "api/hit",
+          analytics: "api/analytics",
+        },
+      ],
+      description: "API HIT MONITORING SERVICE",
+    }),
+  );
+});
 
-app.get('/',(req,res)=>
-{
-    res.status(200).json(ResponceFormatter.sucess({
-        service:"Api Monitoring Service",
-        version:'1.0.0',
-        endpoints:[
-            {
-                health:'/health',
-                auth:'/api/auth',
-                ingest:'api/hit',
-                analytics:'api/analytics'
-            }
-        ],
-            description:"API HIT MONITORING SERVICE"
-        
-    }))
-})
-
-app.use((req,res)=>
-{
-    res.status(404).json(ResponceFormatter.error("Endpoints not found ",404))
-})
+app.use((req, res) => {
+  res.status(404).json(ResponceFormatter.error("Endpoints not found ", 404));
+});
 
 app.use(errorHabler);
 
-async function initilizeConnections()
-{
-try {
-   logger.info("Connecting to MongoDB...");
+async function initilizeConnections() {
+  try {
+    logger.info("Connecting to MongoDB...");
 
-   //connect to mongodb
-   await mongodb.connect();
-   logger.info("Connected to MongoDb");
+    //connect to mongodb
+    await mongodb.connect();
+    logger.info("Connected to MongoDb");
 
-   //connect to postgres
-   await postgres.connect();
-   logger.info("Connected to Postgres");
+    //connect to postgres
+    await postgres.connect();
+    logger.info("Connected to Postgres");
 
-   //connect to the rabbitmq
-   await rabbitmq.connect();
-   logger.info("Connected to Rabbitmq");
+    //connect to the rabbitmq
+    await rabbitmq.connect();
+    logger.info("Connected to Rabbitmq");
 
-   logger.info("All Connections Are Established successfully")
-
-} catch (error) {
-    logger.error("Failed to initializing connections",error);
+    logger.info("All Connections Are Established successfully");
+  } catch (error) {
+    logger.error("Failed to initializing connections", error);
     throw error;
-}
+  }
 }
 
+async function startServer() {
+    try{
+      await initilizeConnections();
+      const server = app.listen(config.port, () => { 
+        logger.info(`server started on port ${config.port}`)
+        logger.info(`Environment:${config.node_env}`);
+        logger.info(`API available at :http://localhost:${config.port}`)
+      });
 
+      const gracefulShutdown = async (signal) => {
+        logger.info(`Received ${signal}. shut down gracefully...`);
+        server.close(async()=>
+        {
+            logger.info("HTTP server closed successfully");
+            await rabbitmq.close();
+            logger.info("Rabbitmq connection closed successfully");
+            await mongodb.disconnect();
+            logger.info("MongoDB connection closed successfully");
+            await postgres.close();
+            logger.info("Postgres connection closed successfully");
+            process.exit(0);
+        });
+        setTimeout(() => {
+            logger.error("Could not close connections in time, forcefully shutting down");
+            process.exit(1);
+        }, 10000);
+      };
+
+        process.on("SIGINT", gracefulShutdown);
+        process.on("SIGTERM", gracefulShutdown);
+
+        //Handle uncaught exceptions
+        process.on("uncaughtException", (error) => {
+            logger.error("Uncaught Exception:", error);
+            gracefulShutdown("uncaughtException");
+        });
+
+        process.on("unhandledRejection", (reason, promise) => {
+            logger.error("Unhandled Rejection at:", promise, "reason:", reason);
+            gracefulShutdown("unhandledRejection");
+        });
+    }
+    catch(error)
+    {
+logger.error("Failed to start server",error);
+    process.exit(1);
+    }
+}
+
+startServer();
