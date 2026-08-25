f3821af8fbe32f007f453e4d6e5f711087bd06a2
unknown <23ai009@sitmng.ac.in>
2026-08-25 15:46:14 +0530

    add the server.js required codes

---
+++ src/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.path}`, {
+    ip: req.ip,
+    userAgent: req.header["user-agent"], //browser info
+  });
+  next();
+});
+
+// Sample route
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+const port = process.env.API_PORT;
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
