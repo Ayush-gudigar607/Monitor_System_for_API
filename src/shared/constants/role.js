119eb8e4503c24063cd159fd209880b59ecc066a
unknown <23ai009@sitmng.ac.in>
2026-08-26 21:24:38 +0530

    add the doc to the role.js

--- src/shared/constants/role.js
+++ src/shared/constants/role.js
@@ -1,20 +1,25 @@
+//roles
 export const ROLES=[
     "SUPER_ADMIN",
     "CLIENT_ADMIN",
     "CLIENT_VIEWER"
 ]
 
-
+// client roles
 export const CLIENT_ROLES=[
     "CLIENT_ADMIN",
     "CLIENT_VIEWER"
 ]
+
+//Application roles
 export const APPLICATION_ROLES={
     SUPER_ADMIN:"SUPER_ADMIN",
     CLIENT_ADMIN:"CLIENT_ADMIN",
     CLIENT_VIEWER:"CLIENT_VIEWER"
 }
 
+//check the isValidClientRole
 export const isValidClientRole=(role)=>CLIENT_ROLES.includes(role);
 
+//check the validRole
 export const isValidRole=(role)=>ROLES.includes(role);
