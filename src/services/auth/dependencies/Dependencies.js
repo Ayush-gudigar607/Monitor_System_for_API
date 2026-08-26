f6110fa027406e0d663949669d538c3429ba3ad2
unknown <23ai009@sitmng.ac.in>
2026-08-26 22:04:05 +0530

    add the doc to the Dependencies.js

--- src/services/auth/dependencies/Dependencies.js
+++ src/services/auth/dependencies/Dependencies.js
@@ -1,17 +1,20 @@
-import {AuthController} from "../controller/authController.js";
-import {AuthService} from "../services/AuthService.js";
+import { AuthController } from "../controller/authController.js";
+import { AuthService } from "../services/AuthService.js";
 import MongoUserRepository from "../repository/UserRepository.js";
 
 class Container {
   static init() {
+    // Initialize the repositories, services, and controllers
     const repositories = {
       userRepository: new MongoUserRepository(),
     };
 
+    // Initialize the services and controllers with their dependencies
     const services = {
       authService: new AuthService(repositories.userRepository),
     };
 
+    // Initialize the controllers with their dependencies
     const controllers = {
       authController: new AuthController(services.authService),
     };
