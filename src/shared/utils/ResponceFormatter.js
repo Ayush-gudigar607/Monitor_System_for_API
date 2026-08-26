930d85c516910916e8eed6a7c78bfcd9c2e658a6
unknown <23ai009@sitmng.ac.in>
2026-08-26 21:08:56 +0530

    add the doc for and spacing responceFormatter.js

--- src/shared/utils/ResponceFormatter.js
+++ src/shared/utils/ResponceFormatter.js
@@ -1,52 +1,51 @@
-class ResponceFormatter{
-    static sucess(data,message="success",statusCode=200)
-    {
-        return {
-            success:true,
-            message,
-            data,
-            statusCode,
-            timeStamp:new Date().toISOString()
-        }
-    }
+class ResponceFormatter {
+  //Response formatter for success response
+  static sucess(data, message = "success", statusCode = 200) {
+    return {
+      success: true,
+      message,
+      data,
+      statusCode,
+      timeStamp: new Date().toISOString(),
+    };
+  }
 
-    static error(message="error",statusCode=500,error=null)
-    {
-        return {
-            success:false,
-            message,
-            error,
-            statusCode,
-            timeStamp:new Date().toISOString()
-        }
-    }
+  //Response formatter for error response
+  static error(message = "error", statusCode = 500, error = null) {
+    return {
+      success: false,
+      message,
+      error,
+      statusCode,
+      timeStamp: new Date().toISOString(),
+    };
+  }
 
-    static validationError(statusCode=400,error=null)
-    {
-        return{
-            success:false,
-            message:"validation error",
-            error,
-            statusCode,
-            timeStamp:new Date().toISOString()
-        }
-    }
+  //Response formatter for validation error response
+  static validationError(statusCode = 400, error = null) {
+    return {
+      success: false,
+      message: "validation error",
+      error,
+      statusCode,
+      timeStamp: new Date().toISOString(),
+    };
+  }
 
-    static paginated(data=null,page,limit,total)
-    {
-        return{
-            sucess:true,
-            data,
-            pagination:{
-                page,
-                limit,
-                total,
-                totalPages:Math.ceil(total/limit)
-            },
-            timeStamp:new Date().toISOString()
-        }
-    }
+  //Response formatter for paginated response
+  static paginated(data = null, page, limit, total) {
+    return {
+      sucess: true,
+      data,
+      pagination: {
+        page,
+        limit,
+        total,
+        totalPages: Math.ceil(total / limit),
+      },
+      timeStamp: new Date().toISOString(),
+    };
+  }
 }
-
 
 export default ResponceFormatter;
