b6ac9e3b110b531562b5b8105728787b0de0531a
unknown <23ai009@sitmng.ac.in>
2026-08-26 21:22:12 +0530

    add the authenticate.js

--- src/shared/middlewares/authenticate.js
+++ src/shared/middlewares/authenticate.js
@@ -1,38 +1,41 @@
 import config from "../config/index.js";
 import ResponceFormatter from "../utils/ResponceFormatter.js";
-import logger from "../config/logger.js"
+import logger from "../config/logger.js";
 import jwt from "jsonwebtoken";
 
-
-const authenticate=(req,res,next)=>
-{
-    try {
-        let token=null;
-        if(req.cookies && req.cookies.token)
-        {
-            token=req.cookies.token
-        }
-        if(!token)
-        {
-            return res.status(401).json(ResponceFormatter.error("Authentication token is required",401))
-        }
-        const decoded =jwt.verify(token,config.jwt.secret);
-        const {_id,username,email,role,clientId}=decoded;
-        req.user={_id,username,email,role,clientId};
-        next();
-    } catch (error) {
-         logger.error("Authentication error:",{
-            error: error.message,
-            path:req.path,
-        })
-
-        if(error.name==="TokenExpiredError")
-        {
-            return res.status(401).json(ResponceFormatter.error("Authentication token has expired",401))
-        }
-    return res.status(401).json(ResponceFormatter.error("Invalid authentication token",401))
-
+//This will authenticate it
+const authenticate = (req, res, next) => {
+  try {
+    let token = null;
+    if (req.cookies && req.cookies.token) {
+      token = req.cookies.token;
     }
-}
+    if (!token) {
+      return res
+        .status(401)
+        .json(ResponceFormatter.error("Authentication token is required", 401));
+    }
+    //decode the jwt
+    const decoded = jwt.verify(token, config.jwt.secret);
+    const { _id, username, email, role, clientId } = decoded;
+    //assign the decoded user information to req.user for further use in the request lifecycle
+    req.user = { _id, username, email, role, clientId };
+    next();
+  } catch (error) {
+    logger.error("Authentication error:", {
+      error: error.message,
+      path: req.path,
+    });
+
+    if (error.name === "TokenExpiredError") {
+      return res
+        .status(401)
+        .json(ResponceFormatter.error("Authentication token has expired", 401));
+    }
+    return res
+      .status(401)
+      .json(ResponceFormatter.error("Invalid authentication token", 401));
+  }
+};
 
 export default authenticate;
