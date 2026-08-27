6a6b36231d64de53309d724ee0bc3dbeba0f1aa0
unknown <23ai009@sitmng.ac.in>
2026-08-27 21:53:29 +0530

    fix the issue in the clientRepository.js

--- src/services/client/repository/ClientRepository.js
+++ src/services/client/repository/ClientRepository.js
@@ -1,40 +1,104 @@
 import BaseClientRepository from "./BaseClientRepository.js";
-import client from "../../../shared/models/Client.js";
-import logger from "../../../shared/config/logger.js";
+import Client from "../../../shared/models/Client.js"
+import logger from "../../../shared/config/logger.js"
 
-class MongoClientRepository extends BaseClientRepository {
-  constructor() {
-    super(client);
-  }
 
-  //This method will create a new client in the database and return the created client object
-  async create(clientData) {
-    try {
-      const existingClient = await this.model.findOne({
-        $or: [{ name: clientData.name }, { slug: clientData.slug }],
-      });
-      if (existingClient) {
-        logger.error(
-          `Client with name ${clientData.name} or slug ${clientData.slug} already exists`,
-        );
-        throw new Error(
-          `Client with name ${clientData.name} or slug ${clientData.slug} already exists`,
-        );
-      }
+class MongoClientRepository extends BaseClientRepository
+{
+    constructor()
+    {
+        super(Client)
+    }
 
-      const client = new this.model(clientData);
-      await client.save();
+    /**
+     * 
+     * @param {OBJECT} clientData 
+     * @returns {Promise<OBJECT>}
+     */
 
-      logger.info("Client created in MongoDB", {
-        mongoId: client._id,
-        name: client.name,
-        slug: client.slug,
-      });
+    async create(clientData)
+    {
+        try {
+            const client=new this.model(clientData)
+            await client.save()
 
-      return client;
-    } catch (err) {
-      logger.error("Error creating client in MongoDB", { error: err.message });
-      throw err;
+
+            logger.info('Client Created in MongoDB',{
+                mongoId:client._id,
+                slug:client.slug
+            })
+            
+            return client
+        } catch (error) {
+            logger.error("Error creating while creation of client",{
+                mongoId:client._id,
+                slug:client.slug
+            })
+            throw error
+        }
+    }
+
+    async findById(clientId)
+    {
+        try {
+            const client=await this.model.findById(clientId)
+            logger.info("Client details from Mongodb",client)
+            return client
+
+        } catch (error) {
+            logger.error("Error finding client in db by id",error)
+            throw error
+        }
+    }
+
+    async findBySlug(slug)
+    {
+        try {
+            const client=await this.model.findOne({slug});
+            logger.info("Client details from Mongodb using Slug",client)
+            return client
+        } catch (error) {
+            logger.error("Error finding client by slug:",error)
+            throw error
+        }
+    }
+    
+
+  /**
+   * @params {object} filters-Query filters
+     @params{Object} options-query options (limit,skip,sort)
+   * @returns {Promise<Object>}
+   */
+
+
+     //after finishing project add the cursor pagination 
+  async find(filters={},options={})
+  {
+    try{
+     const {limit=50,skip=0,sort={createdAt:-1}}=options
+     const clients=await this.model.find(filters).sort(sort).skip(skip).limit(limit).select('__v'); //_v means since id is included by default result look like [{ _id: ..., __v: 0 }, ...]
+     logger.info("Fetch the client by using filter",clients)
+     return clients
+    }
+    catch(err)
+    {
+  logger.error("Error finding Clients:",err);
+  throw err
     }
   }
+
+  async count(filters=[])
+  {
+    try {
+        const count=await this.model.countDocuments(filters)
+        return count
+    } catch (error) {
+   logger.error("Error counting clients:",error)
+   throw error        
+    }
+  }
+
+
 }
+
+export default new MongoClientRepository
