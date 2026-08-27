fba6146ab0ccfd6d1863f4657cc8dd65d2bed6df
unknown <23ai009@sitmng.ac.in>
2026-08-27 21:54:10 +0530

    add the basic one route to creae the client

---
+++ src/services/client/controllers/ClientController.js
@@ -0,0 +1,43 @@
+import ResponseFormatter from "../../../shared/utils/ResponceFormatter.js";
+
+export class ClientController {
+  constructor(clientService, authService) {
+    if (!clientService) {
+      throw new Error("ClientService instance is required");
+    }
+    if (!authService) {
+      throw new Error("AuthService instance is required");
+    }
+
+    this.clientService = clientService;
+    this.authService = authService;
+  }
+
+  async createClient(req, res, next) {
+    try {
+      const isSuperAdmin = await this.authService.checkSuperAdminPermissions(
+        req.user._id,
+      );
+
+      if (!isSuperAdmin) {
+        return res
+          .status(403)
+          .json(
+            ResponseFormatter.error(
+              "You do not have permission to create a client",
+              403,
+            ),
+          );
+      }
+
+      const client = await this.clientService.createClient(req.body, req.user);
+      return res
+        .status(201)
+        .json(
+          ResponseFormatter.sucess(client, "Client created successfully", 201),
+        );
+    } catch (err) {
+      next(err);
+    }
+  }
+}
