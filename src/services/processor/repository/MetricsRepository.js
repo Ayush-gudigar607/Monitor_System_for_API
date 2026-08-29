25992e05cdbc4aebb04cb1e70bc9a231f5667af6
unknown <23ai009@sitmng.ac.in>
2026-08-29 23:02:13 +0530

    add the metricsRepository.js

---
+++ src/services/processor/repository/MetricsRepository.js
@@ -0,0 +1,245 @@
+import { BaseRepository } from "./BaseRepository.js";
+
+// instead of 10000000 records it allows only 1000 records
+const MAX_LIMIT = 1000;
+const DEFAULT_LIMIT = 30000;
+
+export class MetricsRepository extends BaseRepository {
+  constructor({ logger: l, postgres: pg = {} }) {
+    super({ logger: l });
+    if (!pg) {
+      throw new Error("Postgres client is required");
+    }
+    this.postgres = pg;
+  }
+
+  _query(sql,params=[],client=this.postgres)
+  {
+    const target=client || this.postgres;
+
+    if(!target || typeof target.query!=="function")
+    {
+        const err=new Error("Postgres client does not configured properly");
+        this.logger.error(err.message);
+        throw err;
+    }
+
+    return target.query({text:sql,values:params,statement_timeout:QUERY_TIMEOUT_MS});
+  }
+  //UPSERT is being used to continuously accumulate API traffic metrics without creating duplicate rows for the same endpoint/time bucket.
+  async upsertMetrics(metricsData) {
+    try {
+      const {
+        clientId,
+        serviceName,
+        endpoint,
+        method,
+        totalHits,
+        errorHits,
+        avgLatency,
+        minLatency,
+        maxLatency,
+        timeBucket,
+      } = metricsData;
+
+      const query = `INSERT INTO endpoint_metrics(client_id, service_name, endpoint, method, total_hits, error_hits, avg_latency, min_latency, max_latency, time_bucket) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+       ON CONFLICT (client_id, service_name, endpoint, method, time_bucket) 
+       DO UPDATE SET total_hits = endpoint_metrics.total_hits + EXCLUDED.total_hits, 
+       error_hits = endpoint_metrics.error_hits + EXCLUDED.error_hits, 
+       avg_latency = (endpoint_metrics.avg_latency * endpoint_metrics.total_hits + EXCLUDED.avg_latency * EXCLUDED.total_hits) / (endpoint_metrics.total_hits + EXCLUDED.total_hits),
+       min_latency = LEAST(endpoint_metrics.min_latency, EXCLUDED.min_latency), 
+       max_latency = GREATEST(endpoint_metrics.max_latency, EXCLUDED.max_latency)`;
+
+       await this._query(query,[
+        clientId,
+        serviceName,
+        endpoint,
+        method,
+        totalHits,
+        errorHits,
+        avgLatency,
+        minLatency,
+        maxLatency,
+        timeBucket
+       ]);
+
+    } catch (err) {
+        this.logger.error(`Error upserting metrics: ${err.message}`);
+        throw err;
+    }
+  }
+
+  async getMetrics(filter={})
+  {
+    try{
+    const {clientId,serviceName,endpoint,method,startTime,endTime,limit=100,offset=0}=filter;
+const safeLimit = Math.min(
+    Math.max(1, limit),
+    MAX_LIMIT
+);
+    const safeOffset=Math.max(offset,0);
+
+    const query=`SELECT 
+    service_name, endpoint, method,
+    SUM(total_hits) AS total_hits,
+    SUM(error_hits) AS error_hits,
+    AVG(avg_latency) AS avg_latency,
+    MIN(min_latency) AS min_latency,
+    MAX(max_latency) AS max_latency,
+    time_bucket FROM endpoint_metrics`
+        
+
+    const params=[];
+    let paramIndex=1;
+
+    let whereConditions=[];
+
+    if(clientId!=null)
+    {
+        whereConditions.push(`client_id=$${paramIndex}`);
+        params.push(clientId);
+        paramIndex++;
+    }
+
+    if(serviceName)
+    {
+        whereConditions.push(`service_name=$${paramIndex}`);
+        params.push(serviceName);
+        paramIndex++;
+    }
+
+    if(endpoint)
+    {
+        whereConditions.push(`endpoint=$${paramIndex}`);
+        params.push(endpoint);
+        paramIndex++;
+    }
+
+    if(startTime)
+    {
+        whereConditions.push(`time_bucket >= $${paramIndex}`);
+        params.push(startTime);
+        paramIndex++;
+    }
+
+    if(endTime)
+    {
+        whereConditions.push(`time_bucket <= $${paramIndex}`);
+        params.push(endTime);
+        paramIndex++;
+    }
+
+    if(whereConditions.length>0)
+    {
+        query+=` WHERE ${whereConditions.join(" AND ")}`;
+    }
+
+    query+=`GROUP BY service_name, endpoint, method, time_bucket
+     ORDER BY time_bucket DESC 
+     LIMIT $${paramIndex} OFFSET $${paramIndex+1}`;
+
+     params.push(safeLimit,safeOffset);
+     
+     const result=await this._query(query,params);
+     return result.rows;
+  }
+    catch(err)
+    {
+        this.logger.error(`Error getting metrics: ${err.message}`);
+        throw err;
+    }
+  }
+
+  async getEndpoints(clientId,limit=10,startTime=null)
+  {
+    try{
+       const safeLimit=Math.min(Math.max(1,limit),MAX_LIMIT);
+       const query=`
+       SELECT 
+       service_name, endpoint, method,
+       SUM(total_hits) AS total_hits,
+       SUM(avg_latency * total_hits) / NULLIF(SUM(total_hits), 0) as avg_latency,
+       SUM(error_hits) AS error_hits 
+       FROM endpoint_metrics,`;
+
+       const params=[];
+       let paramIndex=1;
+
+       let whereConditions=[];
+
+         if(clientId!=null)
+            {
+                whereConditions.push(`client_id=$${paramIndex}`);
+                params.push(clientId);
+                paramIndex++;
+            }
+        if(startTime)
+        {
+            whereConditions.push(`time_bucket >= $${paramIndex}`);
+            params.push(startTime);
+            paramIndex++;
+        }
+
+        query=`GROUP BY service_name, endpoint, method
+        ORDER BY total_hits DESC
+        LIMIT $${paramIndex}`;
+
+        params.push(safeLimit);
+
+        const result=await this._query(query,params);
+        return result.rows;
+    }
+    catch(err)
+    {
+this.logger.error(`Error getting endpoints: ${err.message}`);
+        throw err;
+    }
+  }
+
+
+  async getOverallstats(clientId,startTime=null,endTime=null)
+  {
+    try{
+        let query=`SELECT 
+        SUM(total_hits) AS total_hits,
+        SUM(error_hits) AS error_hits,
+        SUM(avg_latency * total_hits) / NULLIF(SUM(total_hits), 0) as avg_latency
+        COUNT(DISTINCT service_name) AS unique_services,
+        COUNT(DISTINCT endpoint) AS unique_endpoints
+        FROM endpoint_metrics`;
+
+        const params=[];
+        let paramIndex=1;
+
+        if(clientId!=null)
+        {
+            query+=` WHERE client_id=$${paramIndex}`;
+            params.push(clientId);
+            paramIndex++;
+        }
+
+        if(startTime)
+        {
+            query+=` AND time_bucket >= $${paramIndex}`;
+            params.push(startTime);
+            paramIndex++;
+        }
+
+        if(endTime)
+        {
+            query+=` AND time_bucket <= $${paramIndex}`;
+            params.push(endTime);
+            paramIndex++;
+        }
+
+        const result=await this._query(query,params);
+        return result.rows[0] || {};
+    }
+    catch(err)
+    {
+        this.logger.error(`Error getting overall stats: ${err.message}`);
+        throw err;
+    }
+  }
+}
