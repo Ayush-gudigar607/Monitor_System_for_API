78336d7cbfab56acca1392df2bc2865cc4b60111
unknown <23ai009@sitmng.ac.in>
2026-09-01 21:04:13 +0530

    add the router.js

---
+++ src/services/analytics/routers/analyticService.js
@@ -0,0 +1,20 @@
+import express from "express";
+import analyticsContainer from "../Dependencies/dependencies.js";
+import { AnalyticController } from "../controller/analyticCotroller.js";
+import authenticate from "../../../shared/middlewares/authenticate.js";
+
+const router = express.Router();
+
+router.get(
+  "/status",
+  authenticate,
+  (req,res,next)=>
+  {
+AnalyticController.getStatus(req,res,next)
+  })
+
+router.get("/dashboard", authenticate, (req, res, next) => {
+  AnalyticController.getDashboard(req, res, next);
+});
+
+export default router;
