2d1309d4b71cfe389780c821e3b9d9705d0a40d5
unknown <23ai009@sitmng.ac.in>
2026-09-01 21:04:36 +0530

    add the dependecies.js

---
+++ src/services/analytics/Dependencies/dependencies.js
@@ -0,0 +1,47 @@
+import clientRepository from "../../client/repository/ClientRepository.js";
+import processContainer from "../../../shared/config/processContainer.js";
+import authContainer from "../../auth/dependencies/Dependencies.js";
+
+import {AnalyticsService} from "../services/analyticsService.js";
+import {AnalyticController} from "../controller/analyticCotroller.js";
+
+
+class container
+{
+    static init()
+    {
+        const repositories={
+            cllientRepository:clientRepository,
+            metricsRepository:processContainer.repositories.metricsRepository
+        }
+
+        const analyticsService=new AnalyticsService({
+            metricsRepository:repositories.metricsRepository
+        });
+
+        const services={
+            analyticsService:analyticsService,
+            authService:authContainer.services && authContainer.services.authService,
+        }
+
+        const analyticContainer=new AnalyticController({
+            analyticsService:services.analyticsService,
+            authService:services.authService,
+            clientRepository:repositories.cllientRepository
+        });
+
+        const controllers={
+            analyticController:analyticContainer
+        }
+
+        return {
+            repositories:repositories,
+            services:services,
+            controllers:controllers
+        }
+    }
+}
+
+const initialized=container.init();
+export {container};
+export default initialized;
